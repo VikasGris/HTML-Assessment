8f96e28379395222861c9ce58df94e4c3f036157
VikasGris <vikasgowri0913@gmail.com>
2021-04-03 17:10:55 +0530

    Third Commit

--- Lutosa.js
+++ Lutosa.js
@@ -1,3 +1,18 @@
 function myFunction(x) {
     x.classList.toggle("change");
   }
+  window.addEventListener('scroll', function() {
+    console.log("Scrollin'");
+  });
+  const callback = function(entries) {
+    entries.forEach(entry => {
+      entry.target.classList.toggle("is-visible");
+    });
+  };
+  
+  const observer = new IntersectionObserver(callback);
+  
+  const targets = document.querySelectorAll(".show-on-scroll");
+  targets.forEach(function(target) {
+    observer.observe(target);
+  });
